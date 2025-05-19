143beda9429d5871d5ef7222123db5c44a7ff54c
drezix <andre.t.agostinis@gmail.com>
2025-05-19 10:25:06 -0300

    feat(cors): adicionar cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())
-app.use(cors({
-  origin: 'http://localhost:5000',
+const corsOptions = {
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+};
+
+app.use(cors(corsOptions));
 
 app.use('/archive', archiveRoutes);
 
