26cac8844c5b47555c711ef4d9bc5af2c65b42ac
drezix <andre.t.agostinis@gmail.com>
2025-06-02 09:04:57 -0300

    refactor(src): mover os arquivos do frontend para a pasta src para melhor organização

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,19 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+console.log('Pasta de dados do app:', app.getPath('userData'));
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'renderer.js'),
       nodeIntegration: true,
       contextIsolation: false,
     }
   });
 
-  win.loadFile('index.html');
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
 app.whenReady().then(() => {
